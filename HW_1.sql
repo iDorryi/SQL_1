fb8d91e0225c638f22105013e3f75bfad678ae1a
idorry <iannoyinbox@gmail.com>
2023-04-25 20:24:40 +0300

    First

---
+++ HW_1.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM myfirstdb.hw1
+LIMIT 0, 1000
+
+-- Date: 2023-04-25 20:22
+*/
+INSERT INTO `` (`idhw1`,`product_name`,`manafacturer`,`product_count`,`price`) VALUES (1,'iPhone X','Apple',3,76000);
+INSERT INTO `` (`idhw1`,`product_name`,`manafacturer`,`product_count`,`price`) VALUES (2,'iPhone 8','Apple',2,51000);
+INSERT INTO `` (`idhw1`,`product_name`,`manafacturer`,`product_count`,`price`) VALUES (3,'Galaxy S9','Samsung',2,56000);
+INSERT INTO `` (`idhw1`,`product_name`,`manafacturer`,`product_count`,`price`) VALUES (4,'Galaxy S8','Samsung',1,41000);
+INSERT INTO `` (`idhw1`,`product_name`,`manafacturer`,`product_count`,`price`) VALUES (5,'P20 Pro','Huawei',5,36000);
